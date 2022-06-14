38fa68bee21964cd567a52a8542a5ac55831270f
lalodiazdiaz <eduardodiazdiaz93@gmail.com>
2022-06-14 14:38:25 -0500

    title respomsive

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
-import styles from "./App.module.css";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import { MovieDetails } from "./pages/MovieDetails";
 import { LandingPage } from "./pages/LandingPage";
+import { Header } from "./Components/Header";
 
 export function App() {
   return (
     <Router>
       <header>
         <Link to={"/"}>
-          <h1 className={styles.title}>🍿🍿Movies🍿🍿</h1>
+          <Header />
         </Link>
       </header>
       <main>
---
+++ src/Components/Header.jsx
@@ -0,0 +1,8 @@
+import styles from "./Header.module.css";
+export function Header() {
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>🍿🍿 Movies 🍿🍿</h1>
+    </div>
+  );
+}
